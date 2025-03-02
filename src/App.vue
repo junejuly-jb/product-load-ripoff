f9d97703d4cf39acdebc1eb2cbdb20ac5dcb8473
June Aragoncillo <58539877+junejuly-jb@users.noreply.github.com>
2025-03-03 06:36:38 +0800

    scaffold initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from './_module_product_load.vue'
+</script>
+
+<template>
+  <TheWelcome />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/_module_product_load.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="pl__heading">
+            <h2>Product Load</h2>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.pl__heading{
+    padding: 10px 20px;
+    background-color: rgb(28, 42, 75);
+    color: white;
+}
+</style>

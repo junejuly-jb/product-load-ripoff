b932a4091d7bdc670c8b5d5772dc6527b4f2d0f2
June Aragoncillo <58539877+junejuly-jb@users.noreply.github.com>
2025-03-04 07:08:17 +0800

    initial build

--- src/_module_product_load.vue
+++ src/_module_product_load.vue
@@ -1,7 +1,22 @@
+<script setup>
+    import ExpandCard from './components/ProductLoad/ExpandCard.vue';
+</script>
+
 <template>
     <div>
         <div class="pl__heading">
             <h2>Product Load</h2>
+        </div>
+        
+        <div class="products__container bg_red">
+            <h2>Product List</h2>
+            <div class="w-25">
+                <v-text-field clearable label="Search" variant="underlined"></v-text-field>
+            </div>
+            <ExpandCard/>
+            <ExpandCard/>
+            <ExpandCard/>
+            <ExpandCard/>
         </div>
     </div>
 </template>
@@ -11,5 +26,11 @@
     padding: 10px 20px;
     background-color: rgb(28, 42, 75);
     color: white;
+}
+/* .bg_red{
+    background-color: red;
+} */
+.products__container{
+    margin: 20px 60px 0px 60px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,27 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+
+
 import App from './App.vue'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        aliases,
+        sets: {
+            mdi
+        }
+    },
+})
+const pinia = createPinia()
 
-app.use(createPinia())
-
-app.mount('#app')
+createApp(App).use(vuetify).use(pinia).mount('#app')
